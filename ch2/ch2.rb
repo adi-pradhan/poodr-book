ddfea8763fdce72f4ebb326f587d11a3150dd10f
Adi Pradhan <pradhan.aditya@gmail.com>
2014-03-30 15:01:05 -0400

    code for Chapter 2

--- ch2/ch2.rb
+++ ch2/ch2.rb
@@ -1,23 +1,36 @@
 class Gear
-	attr_reader :chainring , :cog, :rim , :tire
-
-	def initialize(chainring, cog, rim, tire)
-		@chainring = chainring
-		@cog 	   = cog
-		@rim       = rim
-		@tire 	   = tire
-	end
-
-	def ratio
-		chainring/cog.to_f
-	end
-
-	def gear_inches
-		ratio * (rim + (tire*2))
-
-	end
+attr_reader :chainring, :cog, :wheel
+def initialize(chainring, cog, wheel=nil)
+          @chainring = chainring
+          @cog       = cog
+          @wheel     = wheel
+end
+def ratio
+	chainring / cog.to_f
+end
+def gear_inches
+	ratio * wheel.diameter
+end 
 end
 
+class Wheel
+attr_reader :rim, :tire
+def initialize(rim, tire)
+ 	@rim = rim
+	@tire = tire
+end
+def diameter
+	rim + (tire * 2)
+end
+def circumference
+	diameter * Math::PI
+end 
+end
 
-puts Gear.new(52,11,26,1.5).gear_inches
-puts Gear.new(30,27,24,1.25).gear_inches
+@wheel = Wheel.new(26, 1.5) 
+puts @wheel.circumference
+# -> 91.106186954104
+puts Gear.new(52, 11, @wheel).gear_inches 
+# -> 137.090909090909
+puts Gear.new(52, 11).ratio 
+# -> 4.72727272727273
